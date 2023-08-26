d6a1639619be4440674f0f7f7f2a6915250c0678
deepindersinghsindhu <deepinder.sidhu107@gmail.com>
2023-08-26 17:54:51 +0530

    added payment states and events

---
+++ src/main/java/com/practice/SpringStateMachine/domain/PaymentEvent.java
@@ -0,0 +1,10 @@
+package com.practice.SpringStateMachine.domain;
+
+public enum PaymentEvent {
+    PRE_AUTHORIZE,
+    PRE_AUTH_APPROVED,
+    PRE_AUTH_DECLINED,
+    AUTHORIZE,
+    AUTH_APPROVED,
+    AUTH_DECLINED
+}
