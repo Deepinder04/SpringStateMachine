e507a6b7b62e2f8699f44d18fcf890be7291ae03
deepindersinghsindhu <deepinder.sidhu107@gmail.com>
2023-08-26 18:18:06 +0530

    state configuration

---
+++ src/main/java/com/practice/SpringStateMachine/config/StateMachineConfiguration.java
@@ -0,0 +1,26 @@
+package com.practice.SpringStateMachine.config;
+
+import com.practice.SpringStateMachine.domain.PaymentEvent;
+import com.practice.SpringStateMachine.domain.PaymentState;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+
+import java.util.EnumSet;
+
+@EnableStateMachineFactory
+@Slf4j
+@Configuration
+public class StateMachineConfiguration extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent> {
+    @Override
+    public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) throws Exception {
+        states.withStates()
+                .initial(PaymentState.NEW)
+                .states(EnumSet.allOf(PaymentState.class))
+                .end(PaymentState.AUTH)
+                .end(PaymentState.AUTH_ERROR)
+                .end(PaymentState.PRE_AUTH_ERROR);
+    }
+}
